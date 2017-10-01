7752d939ba8b6611a592381273647511b064d84c
taalhach <talhach891@gmail.com>
2017-10-01 22:20:29 +0500

    minor changes

--- TCP_UDP_server.go
+++ TCP_UDP_server.go
@@ -4,13 +4,15 @@ import (
 	"log"
 	"bytes"
 	"fmt"
-	"io"
+
 	"sync"
 	"net"
 )
 var routinecount sync.WaitGroup
 var waitForgoroutine sync.WaitGroup
 var message string
+var ip string="127.0.0.1"
+var port int=5002
 
 func main()  {
 	waitForgoroutine.Add(1)
@@ -23,26 +25,30 @@ func main()  {
 func deployTcpServer()  {
 
 	defer waitForgoroutine.Done()
-	// listens for connection
-	listens,error:=net.Listen("tcp","localhost:5002")
-	fmt.Println("Server is listening: ",listens.Addr())
-	if error!=nil{
-		log.Fatal(error)
+
+
+
+	adr:=net.TCPAddr{
+		Port:port,
+		IP:net.ParseIP(ip),
+
 	}
+	listens,err:=net.ListenTCP("tcp",&adr)
+	fmt.Println("Server is listening: ",listens.Addr())
+	inspectError(err,"Listen Tcp ")
 	// listens for connection
 	for{
-		connection,error:=listens.Accept()
-		if error!=nil {
-			log.Fatal(error)
-			continue
-		}
+		connection,err:=listens.Accept()
+		inspectError(err,"Accept Tcp ")
+
+
 		fmt.Println(connection.RemoteAddr()," connected to TCP server")
 
 
 
 
 
-		go replyTCPClient(connection)
+		go handleTCPClient(connection)
 
 	}
 
@@ -51,27 +57,21 @@ func deployTcpServer()  {
 
 
 
-func replyTCPClient(con net.Conn)  {
+func handleTCPClient(con net.Conn)  {
 	defer con.Close()
 
 	// read is used to read the data from connection stream
 	//we are making a buffer to read data from client for this we have a buffer which read byte by byte
 	buf:=make([]byte,1024)
-	_, err := con.Read(buf)
-	if err!=nil{
-		log.Fatal(err)
+	n, err := con.Read(buf)
+	inspectError(err,"Tcp reading form client ")
 
-	}
 
-	n := bytes.Index(buf, []byte{0})
-	mesage:=string(buf[:n-1]) //
+	mesage:=string(buf[:n]) //
 	fmt.Println(con.RemoteAddr()," says: ",mesage)
 	// writing reply to client
-	_,error1:=io.WriteString(con,"Hey i am server you said: "+mesage)
-	if error1!=nil{
-		log.Fatal(error1)
-		return
-	}
+	_,er:=con.Write([]byte("Hey there"))
+	inspectError(er,"Tcp writing to client ")
 
 	fmt.Println("Closing: ",con.RemoteAddr())
 	fmt.Println("TCP Server Listening at: ",con.LocalAddr())
@@ -92,18 +92,17 @@ func deployUdpserver()  {
 
 
 	addr := net.UDPAddr{
-		Port: 5002,
-		IP:   net.ParseIP("127.0.0.1"),
+		Port: port,
+		IP:   net.ParseIP(ip),
 	}
 	fmt.Println("Waiting and Listen on ", addr.Port)
-	conect, doterror := net.ListenUDP("udp", &addr)
-	if doterror != nil {
-		log.Fatal(doterror)
+	for {
+		conect, err := net.ListenUDP("udp", &addr)
+		inspectError(err, "Listen UDP  ")
+		routinecount.Add(1)
+		go readfromUdpclient(conect)
+		routinecount.Wait()
 	}
-	routinecount.Add(1)
-	go readfromUdpclient(conect)
-	routinecount.Wait()
-	deployUdpserver()
 
 }
 
@@ -111,10 +110,8 @@ func readfromUdpclient(udp_connection *net.UDPConn){
 
 	defer routinecount.Done()
 	bufferreader:=make([]byte,1024)
-	_,adres,erri:=udp_connection.ReadFromUDP(bufferreader)
-	if erri!=nil{
-		log.Fatal(erri)
-	}
+	_,adres,err:=udp_connection.ReadFromUDP(bufferreader)
+	inspectError(err,"Read From udp client ")
 	fmt.Println(adres, "is connected to UDP server ")
 	n:=bytes.Index(bufferreader,[]byte{0})
 	message=string(bufferreader[0:n-1])
@@ -129,9 +126,14 @@ func writetoUDPClient(con *net.UDPConn, adres *net.UDPAddr) {
 
 
 	_, err := con.WriteToUDP([]byte(m), adres)
-	if err != nil {
-		log.Fatal(err)
-	}
+	inspectError(err,"Write to udp  ")
 	con.Close()
 
+}
+
+
+func inspectError(err error,descri string){
+	if err!=nil{
+	log.Fatal(descri+": ",err)
+}
 }
